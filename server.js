aaca8c56ccc89993cdefad7506c1c5913aab1355
Rahul Rudra <rahulrudra665@gmail.com>
2023-06-25 07:56:17 +0600

    new commit

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const app = express();
+var bodyParser = require("body-parser");
+
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Route is Working");
+});
 
 app.listen(3000, (req, res) => {
   console.log("Server is Running");
