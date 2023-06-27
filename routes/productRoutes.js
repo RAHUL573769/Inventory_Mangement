9f76cc9a21b4ba766588434e1eb8505e7d42902e
Rahul Rudra <rahulrudra665@gmail.com>
2023-06-27 07:16:28 +0600

    update done

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const { postProduct, getProduct } = require("../controllers/productController");
+const Product = require("../models/productModel");
 
 const router = express.Router();
 router.post("/", postProduct);
 
 router.get("/", getProduct);
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(req.body);
+    const update = await Product.updateOne({ _id: id }, { $set: req.body });
+    res.send(update);
+  } catch (error) {
+    res.json({
+      message: "Update Failed",
+      status: "failed",
+      error: error.message
+    });
+  }
+});
 
 module.exports = router;
